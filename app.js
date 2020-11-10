f9fcb76c744682d7b6883d9ceb2b22de8a304b33
JanviMahajan14 <janvimahajan337@gmail.com>
2020-11-10 20:18:38 +0530

    Database setup

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./config/keys');
+const PORT = process.env.PORT || 5002;
 
- 
+mongoose.connect(MONGO_URI);
+mongoose.connection.on('connected', () => {
+    console.log("MongoDb is connected!!");
+})
+mongoose.connection.on('error', (err) => {
+    console.log("connecting ", err)
+})
+
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
 })
