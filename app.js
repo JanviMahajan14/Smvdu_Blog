711742fda9eb7812fee258461003172a579b3654
JanviMahajan14 <janvimahajan337@gmail.com>
2020-11-11 15:09:42 +0530

    Authentication

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const { MONGO_URI } = require('./config/keys');
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+});
+
 mongoose.connection.on('connected', () => {
     console.log("MongoDb is connected!!");
 })
 mongoose.connection.on('error', (err) => {
     console.log("connecting ", err)
 })
+
+require("./models/user");
+app.use(express.json());
+app.use(require("./routes/auth"));
 
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
