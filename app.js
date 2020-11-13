e13eba233c236137012b8e80bfa4584de9f1388f
JanviMahajan14 <janvimahajan337@gmail.com>
2020-11-13 17:33:43 +0530

    Added post schema

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ mongoose.connection.on('error', (err) => {
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/auth"));
+app.use(require('./routes/post'));
 
 app.listen(PORT, () => {
     console.log("Server is running on ", PORT);
