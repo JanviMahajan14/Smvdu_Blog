91b3e04a73443279c627b2744ee4648dfb347d7c
JanviMahajan14 <janvimahajan337@gmail.com>
2020-11-29 15:39:01 +0530

    Search user by email functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { userContext } from '../App'
+import M from 'materialize-css';
 
 
 const Navbar = () => {
+    const SearchModal = useRef(null);
+    const [search, setSearch] = useState('');
     const { userState, dispatch } = useContext(userContext);
+    const [userDetails, setuserDetails] = useState([]);
     const history = useHistory();
+
+    useEffect(() => {
+        M.Modal.init(SearchModal.current)
+    }, [])
+    
     const renderList = () => {
         return (
             userState ?
                 <>
-                    <li><i class="material-icons">search</i></li>
+                    <li><i class="modal-trigger material-icons" data-target="modal1" style={{cursor:"pointer"}}>search</i></li>
                     <li><Link to="/explore">Explore</Link></li>
                     <li><Link to="/profile">Profile</Link></li>
                     <li><Link to="/newpost">Add Post</Link></li>
@@ -31,6 +40,22 @@ const Navbar = () => {
         );
     }
 
+    const fetchUsers = async(query) => {
+        setSearch(query)
+        const res = await fetch(`http://localhost:5000/search_user`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                query
+            })
+        })
+        const data = await res.json();
+        setuserDetails(data.user)
+    }
+
     return(
         <nav>
            <div className="nav-wrapper  light-blue darken-1">
@@ -38,7 +63,35 @@ const Navbar = () => {
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     {renderList()}
                </ul>
-           </div>
+            </div>
+            <div id="modal1" className="modal" ref={SearchModal}>
+                <div className="modal-content" style={{ color: "black" }}>
+                    <h4>Search Users</h4>
+                    <input
+                        placeholder="Type here" id="first_name"
+                        type="text"
+                        class="validate"
+                        value={search}
+                        onChange={(e)=>fetchUsers(e.target.value)}
+                    />
+                    <ul className="collection">
+                        {
+                            userDetails.map(item => {
+                                return (
+                                    <li className="collection-item">
+                                        <Link to={"/profile/" + item._id} style={{ color: "black" }} onClick={() => {
+                                            M.Modal.getInstance(SearchModal.current).close()
+                                        }}>{item.email}</Link>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+                <div className="modal-footer">
+                    <a href="#!" className="modal-close waves-effect waves-green btn-flat" onClick={()=>setSearch('')}>Close</a>
+                </div>
+            </div>
        </nav>
     );
 }
--- routes/auth.js
+++ routes/auth.js
@@ -110,4 +110,15 @@ router.post("/user/updatepic", auth, async (req, res) => {
   }
 });
 
+router.post('/search_user', (req, res) => {
+  let userPattern = new RegExp("^" + req.body.query);
+  User.find({ email: { $regex: userPattern } })
+    .select("_id email")
+    .then((user => {
+    res.json({user})
+  })).catch((error) => {
+    console.log(error)
+  })
+})
+
 module.exports = router;
